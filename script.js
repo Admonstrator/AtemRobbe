f55eb0119e7f06158a8a97139a218d3df7353976
Aaron Viehl <hello@admon.in.ua>
2024-04-20 20:58:56 +0200

    Included Title change

--- script.js
+++ script.js
@@ -49,6 +49,7 @@ function startExercise(totalDuration, stages) {
 
         breathingCircle.setAttribute('r', currentRadius);
         breathingStage.textContent = `${stage.action}: ${Math.ceil(stageTimeLeft / 1000)}s`;
+        document.title = `${stage.action}: ${Math.ceil(stageTimeLeft / 1000)}s`;
         totalTimeDisplay.textContent = `Remaining Time: ${Math.ceil(totalTimeLeft / 1000)}s`;
 
         if (totalTimeLeft > 0) {
